66ad3a8b43a9b657cbacc0953054b26e27e2b1ee
cxcx <patrick@bassner.de>
2019-02-15 10:35:00 +1100

    Fix hasFriends

--- src/Client/index.js
+++ src/Client/index.js
@@ -411,7 +411,7 @@ class Client extends Events {
 
       const friends = await this.getFriends();
 
-      return friends.findIndex(friend => friend.accountId === user.id) > -1;
+      return friends.findIndex(friend => friend.id === user.id) > -1;
 
     } catch (err) {
 
